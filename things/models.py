37b71e33c4d0d4a980230354c017d3dbb384d543
Eve Ster <paraskevi.stergiopoulou@kcl.ac.uk>
2022-10-17 18:56:26 +0100

    Migrations

--- things/models.py
+++ things/models.py
@@ -6,17 +6,14 @@ class Thing(models.Model):
         unique=True,
         blank=False,
         max_length=30,
-        null=False,
     )
     description = models.CharField(
         unique=False,
         blank=True,
         max_length=120,
-        null=False,
     )
     quantity = models.IntegerField(
         unique=False,
-        null=False,
         validators=[
             MinValueValidator(0),
             MaxValueValidator(100)
