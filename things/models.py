c1da203fa78c131669c076849ebbde82a843b659
Eve Ster <paraskevi.stergiopoulou@kcl.ac.uk>
2022-10-17 18:48:31 +0100

    null constraint added

--- things/models.py
+++ things/models.py
@@ -3,17 +3,20 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(
-        unique=False,
+        unique=True,
         blank=False,
         max_length=30,
+        null=False,
     )
     description = models.CharField(
-        unique=True,
+        unique=False,
         blank=True,
         max_length=120,
+        null=False,
     )
     quantity = models.IntegerField(
-        unique=True,
+        unique=False,
+        null=False,
         validators=[
             MinValueValidator(0),
             MaxValueValidator(100)
